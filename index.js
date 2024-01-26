bd3b1330f7c9fb4da99cf85ff3f939a63e447ecb
RhythmicRhythm <Okubadejorh@gmail.com>
2024-01-26 19:15:19 +0100

    +

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // cors
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://edu-tech-front.netlify.app"],
+    origin: ["http://localhost:5173", "https://gentle-pixie-121ccb.netlify.app"],
     credentials: true,
   })
 );
